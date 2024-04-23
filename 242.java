fde5ffd5be32c6d14b3d98553c27fb971291f15d
ilias-vas <yakult1032@gmail.com>
2024-04-23 22:40:23 +1000

    Valid Anagram

--- 242.java
+++ 242.java
@@ -1,29 +1,27 @@
-// target integer of n / 2
-// create an empty hash map, loop through the hashmap   
+// Create count hashmap - Increment the values of each character by using a put statement with the key set to the character,
+// and the value set to the value of the current key (or 0 if no value) + 1
+// Decrement the values using string t with the same put statement. Use character as the key, and set the value to the current value - 1
+// Loop through the value of every key using map.values() to check if it is 0 - if it isnt, not an anagram
+// If loop exits, return true since it is an anagram.
 
 import java.util.HashMap;
 
 class Solution {
-    public int majorityElement(int[] nums) {
-        int target = nums.length / 2;
-        HashMap<Integer, Integer> valMap = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            if (valMap.containsKey(nums[i])) {
-                count = valMap.get(nums[i]);
-            }
-            valMap.put(nums[i], count + 1);
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)+1);
         }
 
-        for (HashMap.Entry<Integer, Integer> entry : valMap.entrySet()) {
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            if (value > target) {
-                return key;
-            }
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)-1);
         }
 
-        return 0;
+        for (int amount : map.values()) {
+            if (amount != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
