ab5a1f177ba46f9427c6ee3dfd1c4ae20b7e00e7
ilias-vas <yakult1032@gmail.com>
2024-04-23 18:41:03 +1000

    Update

--- 226.java
+++ 226.java
@@ -1,7 +1,20 @@
 import javax.swing.tree.TreeNode;
 
+import com.sun.source.tree.Tree;
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
         
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        TreeNode currentNode = root.left;
+        root.left = root.right;
+        root.right = currentNode;
+
+        return root;
     }
 }
