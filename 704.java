90624bb386e45df7a8463a82cf379ca9229e573f
ilias-vas <yakult1032@gmail.com>
2024-04-24 11:06:28 +1000

    Binary Search

---
+++ 704.java
@@ -0,0 +1,13 @@
+// Simply loop through array, if current index is equal to target, return target
+// otherwise, return -1
+
+class Solution {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
