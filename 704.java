57c4f2855b978c9ec4842b1b146906205ef8ab51
ilias-vas <yakult1032@gmail.com>
2024-04-24 14:51:14 +1000

    Actually use binary search

--- 704.java
+++ 704.java
@@ -1,11 +1,19 @@
-// Simply loop through array, if current index is equal to target, return target
-// otherwise, return -1
+// binary search algo - left pointer at start, right pointer at end. Calculate the middle (l+r)/2 if the middle is > than the target, move the right pointer to mid - 1.
+// if the middle is < than the target, move the left pointer to m + 1.
+// otherwise we found the target so return that indice
 
 class Solution {
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+        int l = 0;
+        int r = nums.length-1; 
+        while (l <= r) {
+            int m = (l+r)/2;
+            if (nums[m] < target) {
+                l = m+1;
+            } else if (nums[m] > target) {
+                r = m-1;
+            } else {
+                return m;
             }
         }
         return -1;
